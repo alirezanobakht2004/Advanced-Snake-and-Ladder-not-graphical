d694a923fb6094739baa8f51da8519b33a071283
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-05-15 12:38:24 +0430

    diceCompeleting

--- src/main/java/ir/ac/kntu/gamelogic/Dice.java
+++ src/main/java/ir/ac/kntu/gamelogic/Dice.java
@@ -5,20 +5,28 @@ public class Dice {
         int face = RandomHelper.nextInt(8);
         switch (face) {
             case 0:
+                System.out.println(DiceFaces.OneLeft);
                 break;
             case 1:
+                System.out.println(DiceFaces.TwoLeft);
                 break;
             case 2:
+                System.out.println(DiceFaces.OneRight);
                 break;
             case 3:
+                System.out.println(DiceFaces.TwoRight);
                 break;
             case 5:
+                System.out.println(DiceFaces.OneUp);
                 break;
             case 6:
+                System.out.println(DiceFaces.TwoUp);
                 break;
             case 7:
+                System.out.println(DiceFaces.OneDown);
                 break;
             case 8:
+                System.out.println(DiceFaces.TwoDown);
                 break;
             default:
                 break;
